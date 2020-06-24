a11e0fed8d89b2ed26840d5e180186986ed164e3
Victoria Boichenko <vik.boichenko@gmail.com>
2020-06-24 21:08:49 +0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Victoria Boichenko on 24.06.2020.
+//  Copyright © 2020 Victoria Boichenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
